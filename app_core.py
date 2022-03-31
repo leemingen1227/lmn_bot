93abf6a325ea0f6b2bac0fd186383edda908bde9
leemingen1227 <leemingen1227@gmail.com>
2022-03-31 11:12:03 +0800

    edit appcore

--- app_core.py
+++ app_core.py
@@ -8,8 +8,8 @@ from linebot.exceptions import InvalidSignatureError
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('聊天機器人的 Chennel access token')
-handler = WebhookHandler('聊天機器人的 Channel secret')
+line_bot_api = LineBotApi('0wVkfDMGnf1DV1zdj4HcKy59Iua0Xrtdov23X0itf0i6ViAUP6zDU5akEsgUU8AMGmoUkHNsjrwRI7Z3WEwexRvLqpvPO5zhO1VhrDwP3PGEblt4o4AaE8CbiEUR9aYfo1jLEsMS1wc1vDZdkagQUAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('6382fdd65cc543cd0da0e26942f920ca')
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
