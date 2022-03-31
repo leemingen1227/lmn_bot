762431f665ae62f95f90c642fb77926c526c294e
leemingen1227 <leemingen1227@gmail.com>
2022-03-31 14:25:20 +0800

    add linebot.models

--- app_core.py
+++ app_core.py
@@ -4,6 +4,8 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 import configparser
 
 app = Flask(__name__)
